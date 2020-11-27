d913b0694b13ba403db8c4bf9d48cda8e856c4ba
gary <gggggggg@umich.edu>
2020-11-27 16:21:57 -0500

    new collision function

--- highway_env/vehicle/derby.py
+++ highway_env/vehicle/derby.py
@@ -8,6 +8,100 @@ from highway_env.types import Vector
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env import utils
 
+def corner_positions(v: "Vehicle" = None)->np.array:
+    """
+    This method computes the position of each corner with a rotated car.
+
+    """
+    if v is None:
+        print(v)
+        return np.array([[0,0],[0,0],[0,0],[0,0]])
+    p = np.array(v.position)
+    l = v.LENGTH
+    w = v.WIDTH
+    h = v.heading
+
+    c, s = np.cos(h), np.sin(h)
+    r = np.array([[c, -s], [s, c]])
+    corners = np.array([[l*0.5,w*0.5],[-l*0.5,w*0.5],[-l*0.5,-w*0.5],[l*0.5,-w*0.5]])
+
+    for i in range(4):
+        corners[i,:]=r.dot(corners[i,:])+p
+
+    return corners
+
+def find_initial_impact(v1: "Vehicle" = None, v2: "Vehicle" = None)->np.array:
+    """
+    Uses shooter method to find point of initial contact backwards in time
+    """
+    
+    # first find time in past where cars don't intersect
+    
+    c=utils.rotated_rectangles_intersect((v1.position,v1.LENGTH,v1.WIDTH,v1.heading),(v2.position,v2.LENGTH,v2.WIDTH,v2.heading))
+    v1pos = v1.position.copy()
+    v2pos = v2.position.copy()
+    t=.25
+    while c:
+        t+=.25
+        v1pos[0]=v1.position[0]-v1.velocity[0]*t
+        v1pos[1]=v1.position[1]-v1.velocity[1]*t
+        v2pos[0]=v2.position[0]-v2.velocity[0]*t
+        v2pos[1]=v2.position[1]-v2.velocity[1]*t
+        c=utils.rotated_rectangles_intersect((v1pos,v1.LENGTH,v1.WIDTH,v1.heading),(v2pos,v2.LENGTH,v2.WIDTH,v2.heading))
+    
+    t1=0
+    t2=t
+    if utils.rotated_rectangles_intersect((v1.position,v1.LENGTH,v1.WIDTH,v1.heading),(v2.position,v2.LENGTH,v2.WIDTH,v2.heading)):
+        # Shooter method to find exactly when the cars hit
+        while t2-t1>10E-6:
+            t=(t2+t1)/2
+            v1pos[0]=v1.position[0]-v1.velocity[0]*t
+            v1pos[1]=v1.position[1]-v1.velocity[1]*t
+            v2pos[0]=v2.position[0]-v2.velocity[0]*t
+            v2pos[1]=v2.position[1]-v2.velocity[1]*t
+            if utils.rotated_rectangles_intersect((v1pos,v1.LENGTH,v1.WIDTH,v1.heading),(v2pos,v2.LENGTH,v2.WIDTH,v2.heading)):
+                t1 = t
+            else:
+                t2 = t
+
+        v1pos[0]=v1.position[0]-v1.velocity[0]*t1
+        v1pos[1]=v1.position[1]-v1.velocity[1]*t1
+        v2pos[0]=v2.position[0]-v2.velocity[0]*t1
+        v2pos[1]=v2.position[1]-v2.velocity[1]*t1
+        
+        ncorner=0.
+        corner_avg=np.array([0.,0.])
+        vpos1tmp = v1.position.copy()
+        v1.position = v1pos.copy()
+        vpos2tmp = v2.position.copy()
+        v2.position = v2pos.copy()
+        # Determine which corner
+        if utils.has_corner_inside((v1pos,v1.LENGTH,v1.WIDTH,v1.heading),(v2pos,v2.LENGTH,v2.WIDTH,v2.heading)):
+            #V1 insind V2
+            corners = corner_positions(v1)
+            for i in range(4):
+                if utils.point_in_rotated_rectangle(corners[i,:],v2.position,v2.LENGTH,v2.WIDTH,v2.heading):
+                    corner_avg[0]+=corners[i,0]
+                    corner_avg[1]+=corners[i,1]
+                    ncorner+=1.
+        if utils.has_corner_inside((v2pos,v2.LENGTH,v2.WIDTH,v2.heading),(v1pos,v1.LENGTH,v1.WIDTH,v1.heading)):
+            #V2 insind V1
+            corners = corner_positions(v2)
+            for i in range(4):
+                if utils.point_in_rotated_rectangle(corners[i,:],v1.position,v1.LENGTH,v1.WIDTH,v1.heading):
+                    corner_avg[0]+=corners[i,0]
+                    corner_avg[1]+=corners[i,1]
+                    ncorner+=1.
+        corner_avg[0] /= ncorner
+        corner_avg[1] /= ncorner
+        v1.position = vpos1tmp.copy()
+        v2.position = vpos2tmp.copy()
+
+        return corner_avg
+    else:
+        return 0.0
+
+
 class DerbyCar(Vehicle):
     def __init__(self,
                  road: Road,
@@ -32,25 +126,31 @@ class DerbyCar(Vehicle):
         self.did_crash = 0
         other.got_crashed = 0
         other.did_crash = 0
-        if utils.point_in_rotated_rectangle(self.position, other.position, 0.9*other.LENGTH, 0.9*other.WIDTH, other.heading) or utils.point_in_rotated_rectangle(other.position, self.position, 0.9*self.LENGTH, 0.9*self.WIDTH, self.heading):
+
+        #if utils.point_in_rotated_rectangle(self.position, other.position, 0.9*other.LENGTH, 0.9*other.WIDTH, other.heading) or utils.point_in_rotated_rectangle(other.position, self.position, 0.9*self.LENGTH, 0.9*self.WIDTH, self.heading):
+        if utils.rotated_rectangles_intersect((self.position,self.LENGTH*.9,self.WIDTH*.9,self.heading),(other.position,other.LENGTH*.9,other.WIDTH*.9,other.heading)):
             # Determine who hit who (striker is the one with the smallest angle between the line connecting the two centers and their heading)
+            POI=find_initial_impact(self,other)
+            print(POI)
+
             pos_self=np.array(self.position)
             pos_other=np.array(other.position)
-            CenterVector = (pos_other-pos_self)/np.linalg.norm(pos_other-pos_self)
+            CenterVectorSelf = (POI-pos_self)/np.linalg.norm(POI-pos_self)
+            CenterVectorOther = (POI-pos_other)/np.linalg.norm(POI-pos_other)
             if self.speed != 0.0:
                 SelfHVec = np.array(self.velocity,dtype=np.float32)/np.fabs(1.0*self.speed)
             else:
                 SelfHVec = np.array([0,0],dtype=np.float32)
-                SelfHVec[0] += CenterVector[1]
-                SelfHVec[1] += -1.0*CenterVector[0]
+                SelfHVec[0] += CenterVectorSelf[1]
+                SelfHVec[1] += -1.0*CenterVectorSelf[0]
             if other.speed != 0.0:
                 OtherHVec = np.array(other.velocity,dtype=np.float32)/np.fabs(1.0*other.speed)
             else:
                 OtherHVec = np.array([0,0],dtype=np.float32)
-                OtherHVec[0] += CenterVector[1]
-                OtherHVec[1] += -1.0*CenterVector[0]
-            SelfCosAlpha = abs(SelfHVec[0]*CenterVector[0]+SelfHVec[1]*CenterVector[1])
-            OtherCosAlpha = abs(-OtherHVec[0]*CenterVector[0]-OtherHVec[1]*CenterVector[1]) #minus because the vector connecting two cars is pointed towards the "other" car
+                OtherHVec[0] += CenterVectorOther[1]
+                OtherHVec[1] += -1.0*CenterVectorOther[0]
+            SelfCosAlpha = SelfHVec[0]*CenterVectorSelf[0]+SelfHVec[1]*CenterVectorSelf[1]
+            OtherCosAlpha = OtherHVec[0]*CenterVectorOther[0]+OtherHVec[1]*CenterVectorOther[1] #minus because the vector connecting two cars is pointed towards the "other" car
 
             if SelfCosAlpha>OtherCosAlpha:
                 print(self," hit ",other)
@@ -80,3 +180,4 @@ class DerbyCar(Vehicle):
                 other.crash_speed2 = self.crash_speed2
                 c = 1
         return c
+
